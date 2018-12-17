04cecb232fa88e81ad2c3d69505439effe578e7d
James Jackson <James@Grandcircus.co>
2018-12-16 21:51:30 -0500

    James TA Review

--- AHBC.Lab.18/Program.cs
+++ AHBC.Lab.18/Program.cs
@@ -1,45 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AHBC.Lab._18
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            LinkedList<string> strList = new LinkedList<string>();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// James - I like how you are testing this stuff.
+			LinkedList<string> strList = new LinkedList<string>();
 
-            strList.AddFirst("a");
-            strList.AddFirst("B");
-            strList.AddFirst("C");
-            strList.AddFirst("D");
-            strList.AddFirst("E");
+			strList.AddFirst("a");
+			strList.AddFirst("B");
+			strList.AddFirst("C");
+			strList.AddFirst("D");
+			strList.AddFirst("E");
 
-            foreach (var item in strList)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine(" ");
-            Console.WriteLine("break");
-            Console.WriteLine(" ");
+			foreach (var item in strList)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine(" ");
+			Console.WriteLine("break");
+			Console.WriteLine(" ");
 
-            Console.WriteLine(strList.RemoveAt(0));
+			Console.WriteLine(strList.RemoveAt(0));
 
-            foreach (var item in strList)
-            {
-                Console.WriteLine(item);
-            }
+			foreach (var item in strList)
+			{
+				Console.WriteLine(item);
+			}
 
-            Console.WriteLine(" ");
-            Console.WriteLine("BREAK");
-            Console.WriteLine(" ");
+			Console.WriteLine(" ");
+			Console.WriteLine("BREAK");
+			Console.WriteLine(" ");
 
-            strList.PrintReverse();
+			strList.PrintReverse();
 
-            Console.ReadKey();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 }
